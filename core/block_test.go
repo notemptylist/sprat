f3dc1a1caf286fc2360dd4f61f03487b6e554902
regulate <regulate@gmail.com>
2022-12-03 00:04:26 -0800

    Block and header defined

---
+++ core/block_test.go
@@ -0,0 +1,48 @@
+package core
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/notemptylist/sprat/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHeader_Encode_Decode(t *testing.T) {
+	h := &Header{
+		Version:   1,
+		PrevBlock: types.RandomHash(),
+		Timestamp: time.Now().UnixNano(),
+		Height:    10,
+		Nonce:     31337,
+	}
+
+	buf := &bytes.Buffer{}
+	h.EncodeBinary(buf)
+
+	assert.Nil(t, h.EncodeBinary(buf))
+
+	hDecode := &Header{}
+	assert.Nil(t, hDecode.DecodeBinary(buf))
+	assert.Equal(t, h, hDecode)
+}
+
+func TestBlock_Encode_Decode(t *testing.T) {
+	b := &Block{
+		Header: Header{
+			Version:   1,
+			PrevBlock: types.RandomHash(),
+			Timestamp: time.Now().UnixNano(),
+			Height:    10,
+			Nonce:     31337,
+		},
+		Transactions: nil,
+	}
+
+	buf := &bytes.Buffer{}
+	assert.Nil(t, b.EncodeBinary(buf))
+	bDecode := &Block{}
+	assert.Nil(t, bDecode.DecodeBinary(buf))
+	assert.Equal(t, b, bDecode)
+}
